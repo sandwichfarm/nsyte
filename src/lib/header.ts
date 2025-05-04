b4cb49e4155c5ac423a2a5ec4f4cde074e31fac8
dskvr <dskvr@users.noreply.github.com>
2025-05-04 12:31:20 +0200

    header

---
+++ src/lib/header.ts
@@ -0,0 +1,10 @@
+export const header = `
+                             dP            
+                             88            
+88d888b. .d8888b. dP    dP d8888P .d8888b. 
+88'  \`88 Y8ooooo. 88    88   88   88ooood8 
+88    88       88 88.  .88   88   88.  ... 
+dP    dP \`88888P' \`8888P88   dP   \`88888P' 
+                       .88                 
+                   d8888P                  
+`;
