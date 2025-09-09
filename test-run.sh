f2354e293c264792f37e7b172399d7fe2aa714f1
sandwich <dskvr@users.noreply.github.com>
2025-09-09 21:22:45 +0200

    code cleanup

--- test-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Testing compressed file support with debug logging..."
-echo ""
-echo "Starting nsyte run with debug logging enabled..."
-echo ""
-
-NSYTE_LOG_LEVEL=debug ./dist/nsyte run
