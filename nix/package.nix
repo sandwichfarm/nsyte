9bfb18f826c3ddcbaeb1c2ee1a49bdb0847557f7
hzrd149 <github@hzrd149.com>
2026-08-29 11:36:22 +0100

    Package Nix flake with deno2nix

---
+++ nix/package.nix
@@ -0,0 +1,44 @@
+{
+  pkgs,
+  src,
+  version,
+}:
+let
+  inherit (pkgs) lib;
+
+  nsyte = pkgs.buildDenoApplication {
+    pname = "nsyte";
+    inherit version src;
+
+    entrypoint = "src/cli.ts";
+    denoDepsHash = "sha256-lNkreZhZU+rH2QeXFE8ZfwlXRkykJwuqHzTnyfW43CU=";
+
+    runFlags = [
+      "--vendor=true"
+      "--allow-run"
+      "--allow-read"
+      "--allow-write"
+      "--allow-net"
+      "--allow-env"
+      "--allow-sys"
+    ];
+
+    runtimeInputs = [
+      pkgs.git
+      pkgs.which
+      pkgs.libsecret
+      pkgs.xdg-utils
+    ];
+
+    meta = {
+      description = "Publish your site to nostr and blossom servers";
+      homepage = "https://github.com/sandwichfarm/nsyte";
+      license = lib.licenses.mit;
+    };
+  };
+in
+{
+  default = nsyte;
+  inherit nsyte;
+  denoDeps = nsyte.denoDeps;
+}
